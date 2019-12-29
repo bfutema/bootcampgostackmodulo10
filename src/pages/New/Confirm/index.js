6ecca9ca467c99a62c18bb1ba7473d5290ea40ed
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-12-29 18:32:12 -0300

    Selecionando horário

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '~/services/api';
+
+import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 export default function Confirm() {
-  return <View />;
+  return <Background />;
 }
+
+Confirm.navigationOptions = ({ navigation }) => ({
+  title: 'Confirmar',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  ),
+});
